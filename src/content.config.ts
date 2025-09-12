d825e7e776b57f1d678f91928c42374e8cb65bb6
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-09-11 23:09:04 -0300

    feat: configure i18n locales for content

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+export const heroContentSchema = z.object({
+    name: z.string(),
+    surname: z.string(),
+    subtitle: z.string(),
+});
+export type HeroContent = z.infer<typeof heroContentSchema>;
+
+const hero = defineCollection({
+    loader: glob({
+        pattern: "*.json",
+        base: "src/content/hero",
+    }),
+    schema: heroContentSchema,
+});
+
+
+export const collections = { hero };
