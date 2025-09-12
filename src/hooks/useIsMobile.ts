f1d3e20d961298427ce283f34749c785fbcffa9e
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-09-12 00:39:17 -0300

    feat: add useIsMobile react hook for detect user agent

---
+++ src/hooks/useIsMobile.ts
@@ -0,0 +1,14 @@
+import { useCallback } from 'react';
+
+export const useIsMobile = (): () => boolean => {
+  const getIsMobile = useCallback(() => {
+    if (typeof window === 'undefined' || typeof navigator === 'undefined') {
+        return false;
+    }
+
+    const userAgent = navigator.userAgent;
+    return /android|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent.toLowerCase());
+  }, []);
+
+  return getIsMobile;
+};
