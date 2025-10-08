527230d325b362ac8196bb6094da33dec5b1879b
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-10-07 22:23:14 -0300

    chore: update esling config

--- eslint.config.js
+++ eslint.config.js
@@ -2,30 +2,28 @@ import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
-import pluginReact from "eslint-plugin-react";
-import json from "@eslint/json";
-import markdown from "@eslint/markdown";
-import css from "@eslint/css";
-import astroPlugin from "eslint-plugin-astro";
-import prettierPlugin from "eslint-config-prettier";
 import tsParser from "@typescript-eslint/parser";
+import reactPlugin from "eslint-plugin-react";
+import * as astro from "eslint-plugin-astro";
+import tailwind from "eslint-plugin-tailwindcss";
+import prettier from "eslint-config-prettier";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     languageOptions: {
       parser: tsParser,
-      globals: { ...globals.browser, ...globals.node },
       parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
         ecmaFeatures: { jsx: true },
       },
+      globals: { ...globals.browser, ...globals.node },
     },
     plugins: {
-      js,
       "@typescript-eslint": tsPlugin,
-      react: pluginReact,
+      react: reactPlugin,
+      tailwind,
     },
     settings: {
       react: { version: "detect" },
@@ -33,29 +31,11 @@ export default defineConfig([
     rules: {
       ...js.configs.recommended.rules,
       ...tsPlugin.configs.recommended.rules,
-      ...pluginReact.configs.recommended.rules,
-      ...prettierPlugin.rules,
+      ...reactPlugin.configs.recommended.rules,
       "react/react-in-jsx-scope": "off",
       "no-undef": "off",
     },
   },
-  {
-    files: ["**/*.json", "**/*.jsonc", "**/*.json5"],
-    plugins: { json },
-    language: "json/jsonc",
-    rules: { ...json.configs.recommended.rules },
-  },
-  {
-    files: ["**/*.md"],
-    plugins: { markdown },
-    language: "markdown/commonmark",
-    rules: { ...markdown.configs.recommended.rules },
-  },
-  {
-    files: ["**/*.css"],
-    plugins: { css },
-    language: "css/css",
-    rules: { ...css.configs.recommended.rules },
-  },
-  ...astroPlugin.configs.recommended,
+  ...astro.configs.recommended,
+  prettier,
 ]);
