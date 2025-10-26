657191ca5f6556aa734c81712d4ad092733460a5
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-10-26 18:53:09 -0300

    feat(content): update About content

--- src/content/about/schema.ts
+++ src/content/about/schema.ts
@@ -2,6 +2,11 @@ import { z } from "astro:content";
 
 export const AboutContentSchema = z.object({
   title: z.string(),
+  paragraph1: z
+    .string()
+    .refine((intro: string) => intro.includes("{bitcoinCashLink}")),
+  bitcoinCashLink: z.string(),
+  paragraph2: z.string(),
 });
 
 export type AboutContent = z.infer<typeof AboutContentSchema>;
