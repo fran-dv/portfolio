e7aed5b76665bed3f4a70522b77f9a4a709fad5d
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-10-28 23:58:02 -0300

    feat: add content to about section

--- src/content/about/schema.ts
+++ src/content/about/schema.ts
@@ -7,6 +7,14 @@ export const AboutContentSchema = z.object({
     .refine((intro: string) => intro.includes("{bitcoinCashLink}")),
   bitcoinCashLink: z.string(),
   paragraph2: z.string(),
+  callToAction: z
+    .string()
+    .refine(
+      (cta: string) =>
+        cta.includes("{cvLink}") && cta.includes("{contactLink}"),
+    ),
+  cvLink: z.string(),
+  contactLink: z.string(),
 });
 
 export type AboutContent = z.infer<typeof AboutContentSchema>;
