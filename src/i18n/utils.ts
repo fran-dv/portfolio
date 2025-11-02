9f5a4a2fdde54658b5df970f4fa9f3e65f981c65
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-11-01 23:06:47 -0300

    fix(i18n utils): avoid extra slashes in url

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -3,14 +3,15 @@ import { defaultLocale, locales } from "@/i18n/locales";
 const showDefaultLang = true;
 
 export const changeLanguage = (locale: string, path: string): string => {
-  const languagePrefixes = locales;
-  const hasLanguagePrefix = languagePrefixes.some((prefix) =>
-    path.includes(prefix),
-  );
-  if (hasLanguagePrefix) {
-    path = path.replace(new RegExp(`/${languagePrefixes.join("|")}`), "");
-  }
-  return !showDefaultLang && locale === defaultLocale
-    ? path
-    : `/${locale}${path}`;
+  if (!path.startsWith("/")) path = `/${path}`;
+
+  const pattern = new RegExp(`^/(${locales.join("|")})(?=/|$)`);
+  path = path.replace(pattern, "");
+
+  path = path.replace(/\/{2,}/g, "/");
+
+  const newPath =
+    !showDefaultLang && locale === defaultLocale ? path : `/${locale}${path}`;
+
+  return newPath === `/${locale}/` ? `/${locale}` : newPath.replace(/\/$/, "");
 };
