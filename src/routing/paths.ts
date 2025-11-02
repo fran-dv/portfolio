220475bf117a9b95ea43892e0f1ec58fedadfc70
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-11-01 23:03:59 -0300

    fix & feat: fix anchors & add projects path and getRelativeUrl helper function

--- src/routing/paths.ts
+++ src/routing/paths.ts
@@ -1,10 +1,30 @@
+import type { Locale } from "@/i18n/locales";
+import { getRelativeLocaleUrl } from "astro:i18n";
+
 export const Paths = {
   MainPage: "/",
+  Projects: "projects",
 } as const;
 
 export const MainPageAnchors = {
-  Hero: `${Paths.MainPage}#hero`,
-  Projects: `${Paths.MainPage}#projects`,
-  About: `${Paths.MainPage}#about`,
-  Contact: `${Paths.MainPage}#contact`,
+  Hero: "#hero",
+  ProjectsCTABanner: "#projectsCTABanner",
+  About: "#about",
+  Contact: "#contact",
 } as const;
+
+export const getRelativeUrl = (
+  locale: Locale,
+  page: keyof typeof Paths,
+  anchor: keyof typeof MainPageAnchors | undefined = undefined,
+) => {
+  const path = Paths[page];
+  if (anchor) {
+    const urlAnchor = MainPageAnchors[anchor];
+    const url = getRelativeLocaleUrl(locale, path);
+    const urlWithAnchor = url + urlAnchor;
+    return urlWithAnchor;
+  }
+  const url = getRelativeLocaleUrl(locale, path);
+  return url;
+};
