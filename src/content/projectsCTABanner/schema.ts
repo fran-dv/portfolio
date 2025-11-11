94f41752159cbb29ce6bce697b6df4a533b13206
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-11-11 19:25:00 -0300

    refactor: update projectsCTABanner content

--- src/content/projectsCTABanner/schema.ts
+++ src/content/projectsCTABanner/schema.ts
@@ -3,13 +3,6 @@ import { z } from "astro:content";
 export const ProjectsCTABannerContentSchema = z.object({
   title: z.string(),
   ctaButton: z.string(),
-  projects: z.array(
-    z.object({
-      id: z.string(),
-      title: z.string(),
-      description: z.string(),
-    }),
-  ),
 });
 
 export type ProjectsCTABannerContent = z.infer<
